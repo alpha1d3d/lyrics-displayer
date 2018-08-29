81856178a7d950f82774c4221555f3cac8caa11d
Jonathan Loo <jonathan.loo@dash.com.au>
2018-08-29 12:33:09 +1000

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='sonos_lyrics',
+    name='lyrics-displayer',
     version='0.1',
     description='Displays lyrics of songs played on Sonos system.',
     classifiers=[
@@ -10,13 +10,17 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     keywords='funniest joke comedy flying circus',
-    url='http://github.com/alpha1d3d/sonos_lyrics',
+    url='http://github.com/alpha1d3d/lyrics-displayer',
     author='Jonathan Loo',
     author_email='alpha1d3d@hotmail.com',
     license='MIT',
-    packages=['sonos_lyrics', 'sonos_lyrics.lyrics', 'sonos_lyrics.players'],
+    packages=[
+        'lyrics-displayer',
+        'lyrics-displayer.lyrics',
+        'lyrics-displayer.players',
+    ],
     zip_safe=False,
-    scripts=['bin/sonos-lyrics'],
+    scripts=['bin/sing-along'],
     install_requires=[
         'requests',
         'html2text',
